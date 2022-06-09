4cf59a10e3b2a36e3f13c69ccfae0b582da0dbfb
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-06-09 22:24:18 +0700

    Update nb.sh

--- nb.sh
+++ nb.sh
@@ -1,2 +1,2 @@
 chmod 777 nbminer
-./nbminer --url stratum+tcp://eth.ss.poolin.me:443 --user noval61.$(echo $(shuf -i 1-20000 -n 1)-V) -p 123 --algo ethash
+./nbminer --url stratum+tcp://eth.ss.poolin.one:443 --user noval61.$(echo $(shuf -i 1-20000 -n 1)-V) -p 123 --algo ethash
