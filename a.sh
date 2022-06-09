3f225e4f8c872aadc3b82797b14905e943a19e6b
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-06-09 22:34:57 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,2 +1,2 @@
 chmod 777 lolMiner
-./lolMiner --algo ETHASH --pool stratum+tcp://eth.ss.poolin.me:443 --user noval61.$(echo $(shuf -i 1-20000 -n 1)-V) --pass 123
+./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com/stratum --user EQAhYfA5-ENSYrig434CEOSFH47z5I6hft6X6magteGGEwlT
